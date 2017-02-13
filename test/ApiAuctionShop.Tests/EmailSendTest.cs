6cc3721aac95f000f2c82bd0f2f5f1bbc37a41f4
Patryk Sadowski <patsadow2@gmail.com>
2017-02-13 20:41:20 +0100

    test

---
+++ test/ApiAuctionShop.Tests/EmailSendTest.cs
@@ -0,0 +1,20 @@
+﻿using ApiAuctionShop.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApiAuctionShop.Tests
+{
+   
+    public class EmailSend
+    {
+        [Fact]
+        public void EmailSendTestFail()
+        {
+            Assert.Equal(false, EmailSender.SendEmailAsync("test@wp.pl", "test", "test").IsFaulted);
+        }
+    
+    }
+}
